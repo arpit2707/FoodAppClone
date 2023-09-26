f61e61c59e35a50e9e5758d0b327450530101717
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-09-26 23:12:46 +0530

    practice

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,7 +1,11 @@
 import classes from "./Input.module.css";
 
 const Input = (props) => {
-  return <div className={classes.input}></div>;
+  return (
+    <div className={classes.input}>
+      <input></input>
+    </div>
+  );
 };
 
 export default Input;
