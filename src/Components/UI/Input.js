47ac12844617adcf8faaea9f200776196d79d0fa
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-10-04 23:27:28 +0530

    validation

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,6 +1,15 @@
 import classes from "./Input.module.css";
 
 const Input = (props) => {
+  const student1 = {
+    name: "Yash",
+    age: "25",
+  };
+  function changeAge(studentObj) {
+    studentObj.age = "30";
+  }
+  changeAge(student1);
+  console.log(student1.age);
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
